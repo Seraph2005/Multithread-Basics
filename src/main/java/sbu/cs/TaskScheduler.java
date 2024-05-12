2593caad2d3c7e80a25b62a4c8eb6d17d59f30cc
unknown <persianalba@gmail.com>
2024-05-12 19:11:32 +0330

    Tasks finished

--- src/main/java/sbu/cs/TaskScheduler.java
+++ src/main/java/sbu/cs/TaskScheduler.java
@@ -7,9 +7,6 @@ public class TaskScheduler
 {
     public static class Task implements Runnable
     {
-        /*
-            ------------------------- You don't need to modify this part of the code -------------------------
-         */
         String taskName;
         int processingTime;
 
@@ -17,16 +14,14 @@ public class TaskScheduler
             this.taskName = taskName;
             this.processingTime = processingTime;
         }
-        /*
-            ------------------------- You don't need to modify this part of the code -------------------------
-         */
 
         @Override
         public void run() {
-            /*
-            TODO
-                Simulate utilizing CPU by sleeping the thread for the specified processingTime
-             */
+            try {
+                Thread.sleep(processingTime);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
@@ -34,18 +29,24 @@ public class TaskScheduler
     {
         ArrayList<String> finishedTasks = new ArrayList<>();
 
-        /*
-        TODO
-            Create a thread for each given task, And then start them based on which task has the highest priority
-            (highest priority belongs to the tasks that take more time to be completed).
-            You have to wait for each task to get done and then start the next task.
-            Don't forget to add each task's name to the finishedTasks after it's completely finished.
-         */
+        tasks.sort((t1, t2) -> t2.processingTime - t1.processingTime);
+
+        for(Task task : tasks)
+        {
+            Thread thread = new Thread(task);
+            thread.start();
+            try{
+                thread.join();
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            finishedTasks.add(task.taskName);
+        }
 
         return finishedTasks;
     }
 
     public static void main(String[] args) {
-        // Test your code here
+
     }
 }
